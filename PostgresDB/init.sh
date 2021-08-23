c0b9519c4be1c555ffc9b177c509b7e24da1c36f
gandharv <gandharv.suri@iiitb.org>
2021-08-23 16:05:38 +0530

    Kafka connect

--- PostgresDB/init.sh
+++ PostgresDB/init.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-psql -U postgres -d $POSTGRES_DB
-    \c $POSTGRES_DB;
-    BEGIN;
-        CREATE TABLE anon_profiles (id SERIAL PRIMARY KEY, profiledata JSON );
-        ALTER TABLE anon_profiles REPLICA IDENTITY USING INDEX anon_profiles_pkey;
-    COMMIT;
+psql -U postgres -d anonprofile <<-EOSQL
+    CREATE TABLE IF NOT EXISTS anon_profiles (id SERIAL PRIMARY KEY, profiledata JSON );
+    ALTER TABLE anon_profiles REPLICA IDENTITY USING INDEX anon_profiles_pkey;
+EOSQL
+
