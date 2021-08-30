c2f81f559b099f0afd62d70d926a01084ff72ee3
gandharv <gandharv.suri@iiitb.org>
2021-08-30 11:09:49 +0530

    Infrasetup

--- kafka-postgres-connect/connect.sh
+++ kafka-postgres-connect/connect.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -X POST -H "Accept:application/json" -H "Content-Type: application/json" --data @postgresSource.json http://localhost:8083/connectors
+curl -X POST -H "Accept:application/json" -H "Content-Type: application/json" --data @/home/gandharv/MOSIP/reporting/kafka-postgres-connect/postgresSource.json http://localhost:8083/connectors
 
 #Check Connection 
 # curl -X GET -H "Accept:application/json" localhost:8083/connectors/anonprofile-connector
